8b692300c59f5a2ae4941eab2b9dda657648435e
Joost Nijhuis <jnijhuis81@gmail.com>
2019-04-28 14:17:31 +0200

    Added the `DoctrineCollectionSequenceHandler` which is a `SequenceHandlerInterface` and is responsible for handling instances of `Doctrine\Common\Collections\Collection`.

---
+++ src/DoctrineCollectionSequenceHandler.php
@@ -0,0 +1,99 @@
+<?php
+/*
+ * This file is part of the jojo1981/data-resolver-handlers package
+ *
+ * Copyright (c) 2019 Joost Nijhuis <jnijhuis81@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed in the root of the source code
+ */
+namespace Jojo1981\DataResolverHandlers;
+
+use Doctrine\Common\Collections\Collection;
+use Jojo1981\DataResolver\Handler\Exception\HandlerException;
+use Jojo1981\DataResolver\Handler\SequenceHandlerInterface;
+
+/**
+ * @package Jojo1981\DataResolverHandlers
+ */
+class DoctrineCollectionSequenceHandler implements SequenceHandlerInterface
+{
+    /**
+     * @param mixed $data
+     * @return bool
+     */
+    public function supports($data): bool
+    {
+        return $data instanceof Collection;
+    }
+
+    /**
+     * @param mixed|Collection $data
+     * @throws HandlerException
+     * @return \Traversable
+     */
+    public function getIterator($data): \Traversable
+    {
+        if (!$this->supports($data)) {
+            $this->throwUnsupportedException('getIterator');
+        }
+
+        return $data->getIterator();
+    }
+
+    /**
+     * @param mixed|Collection $data
+     * @param callable $callback
+     * @throws HandlerException
+     * @return Collection
+     */
+    public function filter($data, callable $callback): Collection
+    {
+        if (!$this->supports($data)) {
+            $this->throwUnsupportedException('filter');
+        }
+
+        return $data->filter($callback);
+    }
+
+    /**
+     * @param mixed|Collection $data
+     * @param callable $callback
+     * @throws HandlerException
+     * @return Collection
+     */
+    public function flatten($data, callable $callback): Collection
+    {
+        if (!$this->supports($data)) {
+            $this->throwUnsupportedException('flatten');
+        }
+
+        $result = clone $data;
+        $result->clear();
+
+        foreach ($data as $key => $value) {
+            $items = (array) $callback($value, $key);
+            foreach ($items as $item) {
+                $result->add($item);
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param string $methodName
+     * @throws HandlerException
+     * @return void
+     */
+    private function throwUnsupportedException(string $methodName): void
+    {
+        throw new HandlerException(\sprintf(
+            'The `%s` can only handle instances of `%s`. Illegal invocation of method `%s`. You should invoke the `%s` method first!',
+            __CLASS__,
+            Collection::class,
+            $methodName,
+            'supports'
+        ));
+    }
+}
---
+++ tests/DoctrineCollectionSequenceHandlerTest.php
@@ -0,0 +1,256 @@
+<?php
+/*
+ * This file is part of the jojo1981/data-resolver-handlers package
+ *
+ * Copyright (c) 2019 Joost Nijhuis <jnijhuis81@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed in the root of the source code
+ */
+namespace tests\Jojo1981\DataResolverHandlers;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Jojo1981\DataResolver\Handler\Exception\HandlerException;
+use Jojo1981\DataResolverHandlers\DoctrineCollectionSequenceHandler;
+use PHPUnit\Framework\ExpectationFailedException;
+use PHPUnit\Framework\TestCase;
+use Prophecy\Exception\Doubler\ClassNotFoundException;
+use Prophecy\Exception\Doubler\DoubleException;
+use Prophecy\Exception\Doubler\InterfaceNotFoundException;
+use Prophecy\Exception\Prophecy\ObjectProphecyException;
+use Prophecy\Prophecy\ObjectProphecy;
+use SebastianBergmann\RecursionContext\InvalidArgumentException;
+
+/**
+ * @package tests\Jojo1981\DataResolverHandlers
+ */
+class DoctrineCollectionSequenceHandlerTest extends TestCase
+{
+    /** @var ObjectProphecy|Collection */
+    private $originalCollection;
+
+    /** @var ObjectProphecy|Collection */
+    private $returnedCollection;
+
+    /**
+     * @return void
+     * @throws DoubleException
+     * @throws InterfaceNotFoundException
+     * @throws ClassNotFoundException
+     */
+    protected function setUp(): void
+    {
+        $this->originalCollection = $this->prophesize(Collection::class);
+        $this->returnedCollection = $this->prophesize(Collection::class);
+    }
+
+    /**
+     * @test
+     *
+     * @throws InvalidArgumentException
+     * @throws ExpectationFailedException
+     * @return void
+     */
+    public function supportShouldReturnFalseForDataWhichIsNotACollection(): void
+    {
+        $this->assertFalse($this->getDoctrineCollectionSequenceHandler()->supports([]));
+        $this->assertFalse($this->getDoctrineCollectionSequenceHandler()->supports(['key' => 'value']));
+        $this->assertFalse($this->getDoctrineCollectionSequenceHandler()->supports(
+            new \ArrayIterator(['key' => 'value']))
+        );
+    }
+
+    /**
+     * @test
+     *
+     * @throws ObjectProphecyException
+     * @throws InvalidArgumentException
+     * @throws ExpectationFailedException
+     * @return void
+     */
+    public function supportShouldReturnTrueForCollection(): void
+    {
+        $this->assertTrue($this->getDoctrineCollectionSequenceHandler()->supports($this->originalCollection->reveal()));
+    }
+
+    /**
+     * @test
+     *
+     * @throws HandlerException
+     * @return void
+     */
+    public function getIteratorShouldThrowHandlerExceptionWhenCalledAnNotSupportTheData(): void
+    {
+        $this->expectExceptionObject(new HandlerException(
+            'The `' . DoctrineCollectionSequenceHandler::class . '` can only handle instances of ' .
+            '`' . Collection::class . '`. Illegal invocation of method `getIterator`. You should ' .
+            'invoke the `supports` method first!'
+        ));
+
+        $this->getDoctrineCollectionSequenceHandler()->getIterator('Not supported data');
+    }
+
+    /**
+     * @test
+     *
+     * @throws HandlerException
+     * @return void
+     */
+    public function filterShouldThrowHandlerExceptionWhenCalledAnNotSupportTheData(): void
+    {
+        $this->expectExceptionObject(new HandlerException(
+            'The `' . DoctrineCollectionSequenceHandler::class . '` can only handle instances of ' .
+            '`' . Collection::class . '`. Illegal invocation of method `filter`. You should ' .
+            'invoke the `supports` method first!'
+        ));
+
+        $this->getDoctrineCollectionSequenceHandler()->filter('Not supported data', function () {});
+    }
+
+    /**
+     * @test
+     *
+     * @throws HandlerException
+     * @return void
+     */
+    public function flattenShouldThrowHandlerExceptionWhenCalledAnNotSupportTheData(): void
+    {
+        $this->expectExceptionObject(new HandlerException(
+            'The `' . DoctrineCollectionSequenceHandler::class . '` can only handle instances of ' .
+            '`' . Collection::class . '`. Illegal invocation of method `flatten`. You should ' .
+            'invoke the `supports` method first!'
+        ));
+
+        $this->getDoctrineCollectionSequenceHandler()->flatten('Not supported data', function () {});
+    }
+
+    /**
+     * @test
+     *
+     * @throws HandlerException
+     * @throws InvalidArgumentException
+     * @throws ObjectProphecyException
+     * @throws ExpectationFailedException
+     * @return void
+     */
+    public function getIteratorShouldReturnTheIteratorFromTheCollection(): void
+    {
+        $iterator = new \ArrayIterator([]);
+        $this->originalCollection->getIterator()->willReturn($iterator)->shouldBeCalledOnce();
+        $this->assertSame(
+            $iterator,
+            $this->getDoctrineCollectionSequenceHandler()->getIterator($this->originalCollection->reveal())
+        );
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     * @throws ExpectationFailedException
+     * @throws HandlerException
+     * @throws InvalidArgumentException
+     */
+    public function filterShouldReturnTheFilterResultOfTheCollection(): void
+    {
+        $elements = ['key1' => 'value1', 'key2' => 'value2', 'key3' => 'value3', 'key4' => 'value4'];
+        $originalCollection = new ArrayCollection($elements);
+
+        // assert initial data
+        $this->assertEquals(4, $originalCollection->count());
+        $this->assertSame($elements, $originalCollection->toArray());
+
+        // test filter
+        $calledTimes = 0;
+        $expectedCallArguments = [['value1', 'key1'], ['value2', 'key2'], ['value3', 'key3'], ['value4', 'key4']];
+        $callback = function (string $value, string $key) use (&$calledTimes, $expectedCallArguments): bool {
+            $this->assertEquals($expectedCallArguments[$calledTimes], [$value, $key]);
+            $calledTimes++;
+
+            return 'value2' !== $value && 'key3' !== $key;
+        };
+
+        $filteredCollection = $this->getDoctrineCollectionSequenceHandler()->filter($originalCollection, $callback);
+        $this->assertSame(4, $calledTimes, 'Callback is expected to be called exactly 4 times');
+        $this->assertInstanceOf(ArrayCollection::class, $filteredCollection);
+        $this->assertNotSame($filteredCollection, $originalCollection);
+        $this->assertEquals(2, $filteredCollection->count());
+        $this->assertEquals(['key1' => 'value1', 'key4' => 'value4'], $filteredCollection->toArray());
+
+        // assert no side effect are occurred and original collection is not changed
+        $this->assertEquals(4, $originalCollection->count());
+        $this->assertSame($elements, $originalCollection->toArray());
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     * @throws ExpectationFailedException
+     * @throws HandlerException
+     * @throws InvalidArgumentException
+     */
+    public function flattenShouldReturnANewCollectionOfTheSameCollectionClassTypeWithTheFlattenValues(): void
+    {
+        $elements = [
+            'key1' => [
+                'name' => 'value1'
+            ],
+            'key2' => [
+                'name' => [
+                    'value2.1',
+                    'value2.2'
+                ]
+            ],
+            'key3' => [
+                'name' => 'value3'
+            ],
+            'key4' => [
+                'name' => 'value4'
+            ]
+        ];
+        $originalCollection = new ArrayCollection($elements);
+
+        // assert initial data
+        $this->assertEquals(4, $originalCollection->count());
+        $this->assertSame($elements, $originalCollection->toArray());
+
+        // test flatten
+        $calledTimes = 0;
+        $expectedCallArguments = [
+            [['name' => 'value1'], 'key1'],
+            [['name' => ['value2.1', 'value2.2']], 'key2'],
+            [['name' => 'value3'], 'key3'],
+            [['name' => 'value4'], 'key4']
+        ];
+
+        $callback = function (array $value, string $key) use (&$calledTimes, $expectedCallArguments) {
+            $this->assertEquals($expectedCallArguments[$calledTimes], [$value, $key]);
+            $calledTimes++;
+
+            return $value['name'];
+        };
+        $flattenCollection = $this->getDoctrineCollectionSequenceHandler()->flatten($originalCollection, $callback);
+        $this->assertSame(4, $calledTimes, 'Callback is expected to be called exactly 4 times');
+        $this->assertInstanceOf(ArrayCollection::class, $flattenCollection);
+        $this->assertNotSame($flattenCollection, $originalCollection);
+        $this->assertEquals(5, $flattenCollection->count());
+        $this->assertEquals(
+            ['value1', 'value2.1', 'value2.2', 'value3', 'value4'],
+            $flattenCollection->toArray()
+        );
+
+        // assert no side effect are occurred and original collection is not changed
+        $this->assertEquals(4, $originalCollection->count());
+        $this->assertSame($elements, $originalCollection->toArray());
+    }
+
+    /**
+     * @return DoctrineCollectionSequenceHandler
+     */
+    private function getDoctrineCollectionSequenceHandler(): DoctrineCollectionSequenceHandler
+    {
+        return new DoctrineCollectionSequenceHandler();
+    }
+}
